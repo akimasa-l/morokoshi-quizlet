8c56517f9c0e68d40b1d4bbf6ffa02c268ab752b
akimasa-l <liumingzheng8@outlook.com>
2025-01-24 01:23:35 +0900

    Initial Commit

---
+++ morokoshi-quizlet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  morokoshi-quizlet
+//
+//  Created by 劉明正 on 2025/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ morokoshi-quizlet/morokoshi_quizletApp.swift
@@ -0,0 +1,17 @@
+//
+//  morokoshi_quizletApp.swift
+//  morokoshi-quizlet
+//
+//  Created by 劉明正 on 2025/01/24.
+//
+
+import SwiftUI
+
+@main
+struct morokoshi_quizletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
